2117b48633a8fca78521fdc3e43beb5943ef0dfd
jessicacannarozzo <jesscannarozzo@cmail.carleton.ca>
2017-08-28 12:10:27 -0400

    Created functions to save input data

---
+++ public/Scripts/creationScript.js
@@ -0,0 +1,16 @@
+//@Jessica Cannarozzo
+
+//saves question and answers, does not finalize the quiz yet
+function saveQuestion(quizQuestion) {
+  console.log(JSON.stringify(quizQuestion));
+}
+
+//resets all inputs so that new question and answers can be entered
+function refreshQuestion() {
+
+}
+
+//sends quiz back to the server to be stored as a JSON file
+function finalizeQuiz() {
+
+}
