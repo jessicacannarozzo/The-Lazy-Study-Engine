d5b08e135ec9e7f6b8e10f2d2443777600e2f985
Jess <jesscannarozzo@cmail.carleton.ca>
2017-09-05 12:05:40 -0400

    saveQuestion function complete

--- public/Scripts/creationScript.js
+++ public/Scripts/creationScript.js
@@ -1,25 +1,26 @@
 //@Jessica Cannarozzo
 
 var index = 0;
-var finalQuiz = {};
+var finalQuiz = new Object;
 
 //saves question and answers, does not finalize the quiz yet
 function saveQuestion(quizQuestion, quizAnswer, quizAnswer2, quizAnswer3, quizAnswer4) {
-  console.log(finalQuiz);
-  finalQuiz[index] = "";
-  finalQuiz[index].answers = [""];
+  finalQuiz[index] = new Object;
 
+  //set question and solution
   finalQuiz[index].question = quizQuestion;
-  console.log(quizQuestion);
-  console.log(finalQuiz[index].question);
   finalQuiz[index].solution = quizAnswer;
+  finalQuiz[index].answers = [];
 
-  // finalQuiz[index].answers.concat([quizAnswer2, quizAnswer3, quizAnswer4]);
+  //add possible answers
+  finalQuiz[index].answers.push(quizAnswer2); finalQuiz[index].answers.push(quizAnswer3); finalQuiz[index].answers.push(quizAnswer4);
+
   index++;
 
   console.log(JSON.stringify(finalQuiz[index-1]));
 
   //reset question and answers
+  refreshQuestion();
 }
 
 //resets all inputs so that new question and answers can be entered
